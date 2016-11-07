31baa64f9a8fbc742eaa3efbb9e5d8f2dadeb7f7
srikanthraj <raj.srikanth42@gmail.com>
2016-11-06 19:03:24 -0500

    Nov 6 My Guests Location and Host Anno working

--- GuestVite/TrackMyGuestsCellTapped.m
+++ GuestVite/TrackMyGuestsCellTapped.m
@@ -9,20 +9,35 @@
 #import "TrackMyGuestsCellTapped.h"
 #import "MapKit/MapKit.h"
 
-@interface TrackMyGuestsCellTapped ()
+@import Firebase;
+
+@interface TrackMyGuestsCellTapped () <MKMapViewDelegate>
 
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
 @property (weak, nonatomic) IBOutlet UILabel *backLabel;
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
+@property (strong,nonatomic) MKPointAnnotation *guestLocationAnno;
+
+@property (strong,nonatomic) MKPointAnnotation *yourLocationAnno;
+
 
 @property (weak, nonatomic) IBOutlet UINavigationBar *myGuestsLocationBack;
+
+@property(nonatomic,assign) BOOL mapIsMoving;
+
+@property (strong, nonatomic) FIRDatabaseReference *ref;
 
 
 @end
 
 @implementation TrackMyGuestsCellTapped
+
+NSMutableString *hostLatitude;
+NSMutableString *hostLongitude;
+NSMutableString *guestLatitude;
+NSMutableString *guestLongitude;
 
 -(void) viewWillAppear{
     [self.navigationController.navigationBar setFrame:CGRectMake(0, 0, self.view.frame.size.width,80.0)];
@@ -33,8 +48,57 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    self.mapIsMoving  = NO;
+    
+    hostLatitude = [[NSMutableString alloc]init];
+    hostLongitude = [[NSMutableString alloc]init];
+    guestLatitude = [[NSMutableString alloc]init];
+    guestLongitude = [[NSMutableString alloc]init];
+    
+    __block NSMutableString *myHostLatitude = [[NSMutableString alloc]init];
+    
+    __block NSMutableString *myHostLongitude = [[NSMutableString alloc]init];
+    
+    __block NSMutableString *myGuestLatitude = [[NSMutableString alloc]init];;
+    
+    __block NSMutableString *myGuestLongitude = [[NSMutableString alloc]init];;
+    
+    
+     self.ref = [[FIRDatabase database] reference];
+    
     NSLog(@"KEY PASSED IS %@",_key);
    
+    [[[_ref child:@"invites"] child:_key] observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        
+        NSDictionary *dict = snapshot.value;
+        
+        //NSLog(@"DICT %@",dict);
+        
+        myHostLatitude = [NSMutableString stringWithFormat:@"%@",[dict valueForKey:@"Host Latitude"]];
+        myHostLongitude = [NSMutableString stringWithFormat:@"%@",[dict valueForKey:@"Host Longitude"]];
+        myGuestLatitude = [NSMutableString stringWithFormat:@"%@",[dict valueForKey:@"Guest Latitude"]];
+        myGuestLongitude = [NSMutableString stringWithFormat:@"%@",[dict valueForKey:@"Guest Longitude"]];
+        
+        
+    } withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+    }];
+
+    
+    while([myHostLatitude floatValue] == 0.0 && [myHostLongitude floatValue] == 0.0 && [myGuestLatitude floatValue] == 0.0 && [myGuestLongitude floatValue] == 0.0) {
+        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    }
+    
+    hostLatitude = myHostLatitude;
+    hostLongitude = myHostLongitude;
+    guestLatitude = myGuestLatitude;
+    guestLongitude = myGuestLongitude;
+    
+    
+    
+   [self addAnnotations];
+    
+    [self centerMap:self.guestLocationAnno];
     
     
     self.myGuestsLocationBack = [[UINavigationBar alloc]initWithFrame:CGRectMake(0, 0, 400, 64)];
@@ -67,6 +131,37 @@
     [self.view addSubview:_myGuestsLocationBack];
 }
 
+
+-(void) centerMap:(MKPointAnnotation *)centerPoint{
+    
+    [self.mapView setCenterCoordinate:centerPoint.coordinate animated:YES];
+}
+
+
+-(void) addAnnotations {
+    
+    
+    NSLog(@"HOST LATITIDE %f",[hostLatitude floatValue]);
+    NSLog(@"HOST LONGITUDE %f",[hostLongitude floatValue]);
+    NSLog(@"GUEST LATITIDE %f",[guestLatitude floatValue]);
+    NSLog(@"GUEST LONGITUDE %f",[guestLongitude floatValue]);
+    
+    self.yourLocationAnno = [[MKPointAnnotation alloc]init];
+    
+    self.yourLocationAnno.coordinate = CLLocationCoordinate2DMake([hostLatitude floatValue],[hostLongitude floatValue]);
+    
+    self.yourLocationAnno.title = @"Your Location";
+    
+    
+    self.guestLocationAnno = [[MKPointAnnotation alloc]init];
+    
+    self.guestLocationAnno.coordinate = CLLocationCoordinate2DMake([guestLatitude floatValue],[guestLongitude floatValue]);
+    
+    self.guestLocationAnno.title = @"Guest Location";
+    
+    [self.mapView addAnnotations:@[self.yourLocationAnno,self.guestLocationAnno]];
+    
+}
 
 - (IBAction)Back
 {
