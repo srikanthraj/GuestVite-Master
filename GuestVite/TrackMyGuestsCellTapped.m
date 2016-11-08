295cc804261a9e5c7a7c50a540030c901949c99f
srikanthraj <raj.srikanth42@gmail.com>
2016-11-07 20:10:36 -0500

    Nov 7 , In progress

--- GuestVite/TrackMyGuestsCellTapped.m
+++ GuestVite/TrackMyGuestsCellTapped.m
@@ -8,6 +8,8 @@
 
 #import "TrackMyGuestsCellTapped.h"
 #import "MapKit/MapKit.h"
+#import "CNPPopupController.h"
+#import <MessageUI/MessageUI.h>
 
 @import Firebase;
 
@@ -21,6 +23,8 @@
 @property (strong,nonatomic) MKPointAnnotation *guestLocationAnno;
 
 @property (strong,nonatomic) MKPointAnnotation *yourLocationAnno;
+
+@property (nonatomic, strong) CNPPopupController *popupController;
 
 
 @property (weak, nonatomic) IBOutlet UINavigationBar *myGuestsLocationBack;
@@ -114,9 +118,155 @@ NSMutableString *guestLongitude;
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    
+    // IF Guest has NOT YET STARTED from their place, Present a POP UP and give an option to remind the Guest
+    
+     self.ref = [[FIRDatabase database] reference];
+    
+    [[[_ref child:@"invites"] child:_key] observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        
+        NSDictionary *dict = snapshot.value;
+        
+        if([[dict valueForKey:@"Guest Location Status"] isEqualToString:@"NOT_STARTED"])
+        {
+            
+            NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+            paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+            paragraphStyle.alignment = NSTextAlignmentCenter;
+            
+            NSAttributedString *title = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"Looks Like %@ has not yet started from their Location",[dict valueForKey:@"Receiver First Name"]] attributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:24], NSParagraphStyleAttributeName : paragraphStyle}];
+            
+            NSAttributedString *lineOne = [[NSAttributedString alloc] initWithString:@"How about checking back in some time?" attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:18], NSForegroundColorAttributeName : [UIColor colorWithRed:0.46 green:0.8 blue:1.0 alpha:1.0], NSParagraphStyleAttributeName : paragraphStyle}];
+            
+            
+            CNPPopupButton *gotItButton = [[CNPPopupButton alloc] initWithFrame:CGRectMake(0, 0, 200, 60)];
+            [gotItButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            gotItButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+            [gotItButton setTitle:@"OK , Got it!" forState:UIControlStateNormal];
+            gotItButton.backgroundColor = [UIColor colorWithRed:0.46 green:0.8 blue:1.0 alpha:1.0];
+            gotItButton.layer.cornerRadius = 4;
+            gotItButton.selectionHandler = ^(CNPPopupButton *gotItButton){
+                [self.popupController dismissPopupControllerAnimated:YES];
+                
+            };
+            
+            CNPPopupButton *reminderButton = [[CNPPopupButton alloc] initWithFrame:CGRectMake(0, 0, 200, 60)];
+            [reminderButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            reminderButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+            [reminderButton setTitle:@"Send a Reminder" forState:UIControlStateNormal];
+            reminderButton.backgroundColor = [UIColor colorWithRed:0.46 green:0.8 blue:1.0 alpha:1.0];
+            reminderButton.layer.cornerRadius = 4;
+            reminderButton.selectionHandler = ^(CNPPopupButton *reminderButton){
+                
+                
+                // a. Send Message
+                
+                
+                
+                //Check for SMS and Send It
+                
+                if(!([[dict valueForKey:@"Receiver Phone"] isEqualToString:@"BULK"]) && !([[dict valueForKey:@"Receiver Phone"]length] ==0))
+                {
+                    
+                    
+                    
+                    
+                    if(![MFMessageComposeViewController canSendText]) {
+                        UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"GuestVite" message:@"Your Device Does not support SMS" preferredStyle:UIAlertControllerStyleAlert];
+                        
+                        UIAlertAction *aa = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+                        
+                        [ac addAction:aa];
+                        [self presentViewController:ac animated:YES completion:nil];
+                        return;
+                    }
+                    
+                    
+                    
+                    
+                    
+                    NSArray *recipents = [NSArray arrayWithObject:[dict valueForKey:@"Receiver Phone"]];
+                    
+                    
+                    NSString *message = [NSString stringWithFormat:@"Hey %@!,This is reminder for your invite by %@ %@ at their place. Please login/Register to GuestVite App for more Details ,Thanks!",[dict valueForKey:@"Receiver First Name"],[dict valueForKey:@"Sender First Name"],[dict valueForKey:@"Receiver Last Name"]];
+                    
+                    MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+                    messageController.messageComposeDelegate = self;
+                    [messageController setRecipients:recipents];
+                    [messageController setBody:message];
+                    
+                    
+                    [self presentViewController:messageController animated:YES completion:nil];
+                    
+                    
+                }
+                
+               else if(!([[dict valueForKey:@"Receiver EMail"] isEqualToString:@"BULK"]) && !([[dict valueForKey:@"Receiver EMail"]length] ==0))
+                {
+                    
+                    //Send Email
+                    
+                    
+                    // Email Subject
+                    NSString *emailTitle = @"Message From GeuestVite";
+                    // Email Content
+                    NSString *messageBody = [NSString stringWithFormat:@"Hey %@!,This is reminder for your invite by %@ %@ at their place. Please login/Register to GuestVite App for more Details ,Thanks!",[dict valueForKey:@"Receiver First Name"],[dict valueForKey:@"Sender First Name"],[dict valueForKey:@"Receiver Last Name"]];
+                    // To address
+                    NSArray *toRecipents = [NSArray arrayWithObject:[dict valueForKey:@"Receiver EMail"]];
+                    
+                    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+                    mc.mailComposeDelegate = self;
+                    [mc setSubject:emailTitle];
+                    [mc setMessageBody:messageBody isHTML:NO];
+                    [mc setToRecipients:toRecipents];
+                    
+                    // Present mail view controller on screen
+                    [self presentViewController:mc animated:YES completion:NULL];
+                    
+                }
+                
+                
+                
+                [self.popupController dismissPopupControllerAnimated:YES];
+                
+            };
+            
+            
+            
+            
+            UILabel *titleLabel = [[UILabel alloc] init];
+            titleLabel.numberOfLines = 0;
+            titleLabel.attributedText = title;
+            
+            UILabel *lineOneLabel = [[UILabel alloc] init];
+            lineOneLabel.numberOfLines = 0;
+            lineOneLabel.attributedText = lineOne;
+            
+            self.popupController = [[CNPPopupController alloc] initWithContents:@[titleLabel, lineOneLabel,buttonYesMessage,buttonYes,buttonNoMessage]];
+            self.popupController.theme = [CNPPopupTheme defaultTheme];
+            self.popupController.theme.popupStyle = CNPPopupStyleCentered;
+            self.popupController.delegate = self;
+            [self.popupController presentPopupControllerAnimated:YES];
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+    } withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+    }];
+    
+
+    
+    
+    //----------------------------
+    
     self.mapIsMoving  = NO;
     self.mapView.camera.altitude *= 1.4;
-    
     [self refreshLocation];
     
     self.myGuestsLocationBack = [[UINavigationBar alloc]initWithFrame:CGRectMake(0, 0, 400, 64)];
